347fab7e8f83e543a85855f5a2b7da0b6ad50feb
Frank van Luling <octillerwie@gmail.com>
2020-11-13 14:53:16 +0100

    added the upload image and show part

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
   # accepts_nested_attributes_for :doses
 end
